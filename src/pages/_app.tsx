4c334c7279e9a72f1e6bef05d74aa02e7f46d9f3
th-release <cth.release@gmail.com>
2023-02-20 22:46:58 +0900

    feat: add GlobalStyles, Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
+import { Head } from '@/components/Head'
+import { GlobalStyles } from '@/styles/globals'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Head />
+      <Component {...pageProps} />
+    </>
+  )
 }
